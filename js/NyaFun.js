d10bc7f6a85e3ae932a95b3fed3eb79b5a2f004a
PowerTechott <kenong6635@gmail.com>
2024-08-13 22:48:56 +0800

    Update NyaFun.js

--- js/NyaFun.js
+++ js/NyaFun.js
@@ -5,7 +5,7 @@ var rule = {
     host: 'https://www.nyafun.net',
     homeUrl:'/map.html',
 	url: '/index.php/api/vod#type=fyclass&page=fypage',
-    class_name:'番剧&剧场',
+    class_name:'动画',
     class_url:'2&1',
     detailUrl:'/bangumi/fyid.html',
     推荐:'.border-box .public-list-box;a&&title;.lazy&&data-src;.public-list-prb&&Text;a&&href',
